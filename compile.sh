c29faeb1d26d6c1fea537d404e20d226aa6a5e63
hinest <treverhines@gmail.com>
2017-07-27 16:40:36 -0400

    commit before merging bibs

--- compile.sh
+++ compile.sh
@@ -5,5 +5,5 @@ bibtex ch2/ch2.aux
 bibtex ch3/ch3.aux
 bibtex ch4/ch4.aux
 bibtex ch5/ch5.aux
-bibtex ch5_appendix/ch5_appendix.aux
+bibtex ch6/ch6.aux
 pdflatex -synctex=1 -interaction=nonstopmode dissertation.tex
